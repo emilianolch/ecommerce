a13e8379c6017b2e200cfc63361f409457ed3c53
Emiliano López <emilianolch@gmail.com>
2022-07-09 21:11:30 -0300

    remove items from cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,13 +1,17 @@
 class CartController < ApplicationController
   def index
+    @products = session[:cart].map { |id| Product.find(id) }
+    redirect_to products_path if @products.empty?
   end
 
   def add
     product = Product.find(params[:id])
-    session[:cart] << product
+    session[:cart] << product.id
     redirect_to products_path, notice: "#{product.name} has been added to cart"
   end
 
   def remove
+    session[:cart].delete_at(params[:index].to_i)
+    redirect_to cart_index_path
   end
 end
